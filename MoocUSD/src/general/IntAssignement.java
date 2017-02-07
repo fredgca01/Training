7130fa44917d8c730353f84acfdbec40f8c6980e
frederic.garcia <frederic.garcia@LAPTOP146.hq.transatel.com>
2017-02-07 15:00:23 +0100

    First exo

--- MoocUSD/src/general/IntAssignement.java
+++ MoocUSD/src/general/IntAssignement.java
@@ -4,7 +4,9 @@ public class IntAssignement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int var1=0;
+		int var2 = var1+1;
+		System.out.println("var1: "+var1+" var2: "+var2);
 	}
 
 }
