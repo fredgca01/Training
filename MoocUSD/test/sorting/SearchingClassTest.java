2f33c72cbabf4fd6d07dce1a0551c4b1634155e1
frederic.garcia <frederic.garcia@LAPTOP146.hq.transatel.com>
2017-02-10 11:17:26 +0100

    Adding JUnitTests

---
+++ MoocUSD/test/sorting/SearchingClassTest.java
@@ -0,0 +1,36 @@
+package sorting;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SearchingClassTest {
+	public List<Airport> data;
+	Airport result;
+	
+	@Before
+	public void setUp() throws Exception {
+		data=Arrays.asList(new Airport("Berlin"), new Airport("Catane"), new Airport("New york"), new Airport("Moscow"), new Airport("Paris"));
+	}
+
+	@Test
+	public void testLinearSearch() {
+		result=SearchingClass.linearSearch("Paris",data);
+		assertTrue("LinearSearch trouve", "Paris".equals(result.getShortName()));
+		result=SearchingClass.linearSearch("Caen",data);
+		assertNull("LinearSearch ne trouve pas", result);
+	}
+
+	@Test
+	public void testBinarySearch() {
+		result=SearchingClass.binarySearch("Paris",(Airport[]) data.toArray());
+		assertTrue("LinearSearch trouve", "Paris".equals(result.getShortName()));
+		result=SearchingClass.binarySearch("Caen",(Airport[]) data.toArray());
+		assertNull("LinearSearch ne trouve pas", result);
+	}
+
+}
