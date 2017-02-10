eb24346c741f61e872c57aed0ff7179a269e362f
frederic.garcia <frederic.garcia@LAPTOP146.hq.transatel.com>
2017-02-10 17:31:03 +0100

    Autre exos

--- MoocUSD/src/general/MainClass.java
+++ MoocUSD/src/general/MainClass.java
@@ -20,9 +20,19 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		creditCardTester();
+		ArrayListMethodTester();
 	}
 	
+	
+	public static void test3(){
+		String[] data = {"Paris", "Rotterdam", "Amsterdam", "Rennes", "Caen", "Moscow", "Bamako", "Toulouse"};
+		String result=null;
+		for (String city : data){
+			if(result==null || city.length()<result.length())
+				result=city;
+		}
+		System.out.println(result);
+	}
 	
 	
 	public static void test1() {
@@ -136,6 +146,72 @@ public class MainClass {
         visa.purchase(5);
         System.out.println(visa.getBalance());
         System.out.println("Expected: 3.49");
+	}
+	
+	public static void ArrayListMethodTester() {
+		//set up
+        ArrayList<String> animals = new ArrayList<String>();
+        ArrayListMethods zoo = new ArrayListMethods(animals); 
+        zoo.list.add("ape");
+        zoo.list.add("dog");
+        zoo.list.add("zebra");
+        
+        //test isSorted
+        System.out.println(zoo.isSorted());
+        System.out.println("test isSorted, Expected: true");
+        
+        zoo.list.add("cat");
+        System.out.println(zoo.isSorted());
+        System.out.println("test isSorted, Expected: false");
+        
+        zoo.list.remove("cat");
+        zoo.list.add(0,"cat");
+        System.out.println(zoo.isSorted());
+        System.out.println("test isSorted, Expected: false");
+        
+        
+        
+        zoo.list.add(3,"horse");
+        
+        //test replaceWithLargerNeighbor
+        zoo.replaceWithLargerNeighbor();
+        System.out.println(zoo.toString());
+        System.out.println("replaceWithLargerNeighbor, Expected: [cat, dog, horse, zebra, zebra]");
+        
+        zoo.list.add(0,"cat");
+        
+        //test count duplicates;
+        System.out.println(zoo.countDuplicates());
+        System.out.println("countDuplicates, Expected: 2");
+        zoo.list.add(4,"dog");
+        System.out.println(zoo.countDuplicates());
+        System.out.println("countDuplicates, Expected: 3");
+        
+        
+        //start with fresh array list
+        animals = new ArrayList<String>();
+        zoo = new ArrayListMethods(animals); 
+        
+        //does empty array list work?
+         System.out.println(zoo.isSorted());
+         System.out.println("isSorted, Expected: true");
+         zoo.replaceWithLargerNeighbor();
+         System.out.println(zoo.toString());
+         System.out.println("replaceWithLargerNeighbor, Expected: []");
+         System.out.println(zoo.countDuplicates());
+         System.out.println("countDuplicates, Expected: 0");
+        
+        //test xyzToFront
+        zoo.list.add("ape");
+        zoo.list.add("dog");
+        zoo.list.add("xantus");
+        zoo.list.add("zebra");
+        zoo.list.add("cat");
+        zoo.list.add("yak");
+        
+        zoo.xyzToFront();
+        System.out.println(zoo.toString());
+        System.out.println("Expected: [xantus, zebra, yak, ape, dog, cat]");
 	}
 	
 
