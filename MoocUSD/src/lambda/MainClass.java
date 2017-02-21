2eea91b615cfd851fb049fbbc874830d90f87b4a
frederic.garcia <frederic.garcia@LAPTOP146.hq.transatel.com>
2017-02-21 14:34:21 +0100

    Lambda

--- MoocUSD/src/lambda/MainClass.java
+++ MoocUSD/src/lambda/MainClass.java
@@ -6,7 +6,24 @@ public class MainClass {
 	 * http://www.supinfo.com/articles/single/1467-introduction-java-8-expressions-lambda-interface-fonctionnelle
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Integer result;
+		result = calculate(10, 11, new Addition());
+		System.out.println(result);
+		result = calculate(10, 11, new Multiplication());
+		System.out.println(result);
+		result = calculate(10, 11, new Soustraction());
+		System.out.println(result);
+		
+		//Definition d'une lambda expression. Pas de creation d'objet
+		Operation division = (x,y) -> x/y;
+		
+		result = calculate(4, 2, division);
+		System.out.println("Division via Lambda :" + result);
+		
+		
 	}
 
+	public static Integer calculate (Integer x, Integer y, Operation ope){
+		return ope.calculer(x, y);
+	}
 }
---
+++ MoocUSD/src/lambda/PrintNames.java
@@ -0,0 +1,44 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PrintNames {
+
+	//one method per interface
+	@FunctionalInterface
+	public interface Condition <T> {
+		public boolean toPrint (T element);
+		//public boolean toDisplay (T element);
+	}
+	
+	// Without Lambda
+	/*public class ConditionImpl implements Condition<String> {
+		@Override
+		public boolean toPrint (String element) {
+			// La seule ligne réellement utile !
+            return element.length() <= 5;
+		}
+	}*/
+	
+	public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("un", "deux", "trois", "quatre");
+        
+        //without Lambda
+        /*PrintNames.ConditionImpl isShortName  = new PrintNames().new ConditionImpl();
+        for (String element : names) {
+        	if (isShortName.toPrint(element))
+        		System.out.println(element);
+        }*/
+        
+        //lambda
+        Condition<String> conditionString = (x) -> x.length() <= 5;
+        
+        for (String element : names) {
+        	if (conditionString.toPrint(element))
+        		System.out.println(element);
+        }
+
+	}
+}
