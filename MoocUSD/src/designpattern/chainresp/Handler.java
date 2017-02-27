c24c68b585f30d730eafb0d27ee8c46c6dd62be4
frederic.garcia <frederic.garcia@LAPTOP146.hq.transatel.com>
2017-02-27 18:36:52 +0100

    COR design pattern

--- MoocUSD/src/designpattern/chainresp/Handler.java
+++ MoocUSD/src/designpattern/chainresp/Handler.java
@@ -3,30 +3,30 @@ package designpattern.chainresp;
 import java.util.Random;
 
 public class Handler {
-	private static Random s_rn = new Random();
-    private static int s_next = 1;
+	private static Random randomData = new Random();
+    private static int next = 1;
     
-    private int m_id = s_next++;
-    private Handler m_next;
+    private int id = next++;
+    private Handler nextHandler;
 
     public void add(Handler next) {
-        if (m_next == null)
-          m_next = next;
+        if (nextHandler == null)
+        	nextHandler = next;
         else
-          m_next.add(next);
+        	nextHandler.add(next);
     }
     public void wrap_around(Handler root) {
-        if (m_next == null)
-          m_next = root;
+        if (nextHandler == null)
+        	nextHandler = root;
         else
-          m_next.wrap_around(root);
+        	nextHandler.wrap_around(root);
     }
     public void handle(int num) {
-        if (s_rn.nextInt(4) != 0) {
-            System.out.print(m_id + "-busy  ");
-            m_next.handle(num);
+        if (randomData.nextInt(4) != 0) {
+            System.out.print(id + "-busy  ");
+            nextHandler.handle(num);
         }
         else
-          System.out.println(m_id + "-handled-" + num);
+          System.out.println(id + "-handled-" + num);
     }
 }
