b35c5661405548d9f550050e51c0dc34da86a07c
frederic.garcia <frederic.garcia@LAPTOP146.hq.transatel.com>
2017-02-28 11:29:44 +0100

    comment

--- MoocUSD/src/designpattern/chainresp/PurchasePower.java
+++ MoocUSD/src/designpattern/chainresp/PurchasePower.java
@@ -1,5 +1,16 @@
 package designpattern.chainresp;
 
+
+/**
+ * The pattern chains the receiving objects together, and then passes any request messages from object 
+ * to object until it reaches an object capable of handling the message. 
+ * The number and type of handler objects isn't known a priori, they can be configured dynamically. 
+ * The chaining mechanism uses recursive composition to allow an unlimited number of handlers to be linked.
+ * Chain of Responsibility simplifies object interconnections. Instead of senders and receivers maintaining 
+ * references to all candidate receivers, each sender keeps a single reference to the head of the chain, 
+ * and each receiver keeps a single reference to its immediate successor in the chain.
+ *
+ */
 public abstract class PurchasePower {
 	protected static final double BASE = 500;
     protected PurchasePower successor;
