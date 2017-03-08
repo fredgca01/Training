7c1b43a94fda06d6a0fd6675d4bd48ce53ca499d
frederic.garcia <frederic.garcia@LAPTOP146.hq.transatel.com>
2017-03-08 12:49:17 +0100

    ajout d'une trace pour voir interets par an

--- MoocUSD/src/language/CompoundInterest.java
+++ MoocUSD/src/language/CompoundInterest.java
@@ -6,8 +6,8 @@ public class CompoundInterest {
 	
 	public static void main(String[] args) {
 		// taxes CTO:0,6 avant IR; AV:0,85 avant IR
-		calculate(260000, 5000, 6, 18,0.85);
-		calculate(1000, 1000, 6, 16,0.85);
+		calculate(260000, 5000, 6, 15,0.6);
+		//calculate(1000, 1000, 6, 16,0.85);
 	}
 	
 	public static void calculate(double principal, double income, double rate, int years, double taxes) {
@@ -19,14 +19,14 @@ public class CompoundInterest {
 			double result = amount * rate/100;
 			//Taxes
 	        result = result * taxes;
-	        
+	        System.out.println("Yearly dividends: "+result);
 	        amount = amount + result;
 	        
 	        //adding yearly income
 	        amount = amount + income;
 	    }
-		//Taxe prelevement lib de 7.5% pour l'AV
-		System.out.println(decimalFormat.format(amount - amount*0.075));
 		
+		//Taxe prelevement lib de 7.5% pour l'AV
+		System.out.println("Total: "+decimalFormat.format(amount - amount*0.075));
 	}
 }
